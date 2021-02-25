51a5c06fe66c8682b4e136358f8af054f6555a39
Arshay Rao <rao.arshay@gmail.com>
2021-02-25 10:36:12 -0800

    Finished Lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'mhpDSHygPocCVq2UMM6ooMxIRUMgah0Y';
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
